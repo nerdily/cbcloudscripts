a9f538471069de95adcc3cb4b18f05e68559887c
nerdily <bmwftw@icloud.com>
2023-02-23 12:01:22 -0500

    Refactored to require command line arguments instead of baking keys into the script

--- endpoints/export-endpoints.py
+++ endpoints/export-endpoints.py
@@ -1,66 +1,118 @@
-import pandas as pd
 import requests
+import argparse
+import sys
+import pandas as pd
 
-# This script exports the queried for endpoints into a csv. To change the query, look at the "data" variable which is the
+# This script exports the queried for endpoints into a csv. To change the query, look at the "payload" variable which is the
 # json-formatted request made to the CB Cloud back end. The developer documentation has a full list of what can be queried.
 # The CB Cloud API will return up to 10,000 items in a single request. If you have more than 10,000 endpoints, you would need
 # multiple requests to fetch them all.
 
+# Usage: python export-endpoints.py --help
+
 # API key permissions required:
 # Device - General Information - device - read
 
-api_id = 'api_id'
-api_secret_key = 'api_secret'
-org_key = 'org_key'
-org_id = 'org_id'
-base_url = 'https://defense.conferdeploy.net'
 
-# Alternatively you can prompt each time for information:
-# api_id = input('Enter the CBC API ID: ')
-# api_secret_key = input('Enter the CBC API Key: ')
-# org_key = input('Enter the CBC Org Key: ')
-# base_url = input('Enter the URL of your Carbon Black Cloud instance: ')
+def get_environment(environment):
+    # Function to get the required environment to build a Base URL. More info about building a Base URL can be found at
+    # https://developer.carbonblack.com/reference/carbon-black-cloud/authentication/#building-your-base-urls
 
-api_token = '/'.join([api_secret_key, api_id])
+    # rtype: string
 
-print('Your Parameters:')
-print('API ID: ' + api_id)
-print('Secret: ' + api_secret_key)
-print('Org key: ' + org_key)
-print('Base URL: ' + base_url)
-print('API token: ' + api_token)
+    if environment == "EAP1":
+        return "https://defense-eap01.confer.deploy.net"
+    elif environment == "PROD01":
+        return "https://dashboard.confer.net"
+    elif environment == "PROD02":
+        return "https://defense.conferdeploy.net"
+    elif environment == "PROD05":
+        return "https://defense-prod05.conferdeploy.net"
+    elif environment == "PROD06":
+        return "https://defense-eu.conferdeploy.net"
+    elif environment == "PRODNRT":
+        return "https://defense-prodnrt.conferdeploy.net"
+    elif environment == "PRODSYD":
+        return "https://defense-prodsyd.conferdeploy.net"
+    elif environment == "PRODUK":
+        return "https://ew2.carbonblack.vmware.com"
+    elif environment == "GOVCLOUD":
+        return "https://gprd1usgw1.carbonblack-us-gov.vmware.com"
 
-# Documentation on this specific API call can be found here:
-# https://developer.carbonblack.com/reference/carbon-black-cloud/platform/latest/devices-api/
 
-# Form our request URL:
-req_url = base_url + '/appservices/v6/orgs/' + org_key + '/devices/_search'
+def build_base_url(environment, org_key):
+    # Build the base URL
+    # rtype: string
 
-# Set our headers
-headers = {'X-Auth-Token': api_token,
-           'Content-Type': 'application/json'
-           }
-# Set our data (body) for the request
-data = {"criteria":{"deployment_type":["ENDPOINT","WORKLOAD","VDI"]},"sort":[{"field":"last_contact_time","order":"DESC"}],"start":1,"rows":10000,"":""}
+    environment = get_environment(environment)
+    return f"{environment}/appservices/v6/orgs/{org_key}/devices/_search"
 
-# Double check they're ok
-print('Request URL: ' + req_url)
-print('Headers: ', end="")
-print(headers)
-print('Data: ', end="")
-print(data)
 
-# Make the request
-req = requests.post(req_url, headers=headers, json=data)
-print('Status code: ' + str(req.status_code))
+def main():
+    # Main function to parse arguments and retrieve the endpoint results
 
-devices_dict = req.json()
-devices = pd.DataFrame.from_dict(devices_dict['results'])
-devices.set_index('device_owner_id', drop=True, inplace=True)
+    parser = argparse.ArgumentParser(prog="export-endpoints.py",
+                                     description="Query VMware Carbon Black \
+                                         Cloud for endpoint data.")
+    requiredNamed = parser.add_argument_group('required arguments')
+    requiredNamed.add_argument("-e", "--environment", required=True, default="PROD05",
+                        choices=["EAP1", "PROD01", "PROD02", "PROD05",
+                                 "PROD06", "PRODNRT", "PRODSYD", "PRODUK", "GOVCLOUD"],
+                        help="Environment for the Base URL")
+    requiredNamed.add_argument("-o", "--org_key", required=True,
+                        help="Org key (found in your product console under \
+                              Settings > API Access > API Keys)")
+    requiredNamed.add_argument("-i", "--api_id", required=True,
+                        help="API ID")
+    requiredNamed.add_argument("-s", "--api_secret", required=True,
+                        help="API Secret Key")
+    args = parser.parse_args()
 
-print('Total devices found: ', end="")
-print(devices_dict.get('num_found'))
+    req_url = build_base_url(args.environment, args.org_key)
+    api_token = f"{args.api_secret}/{args.api_id}"
 
-# Cool. Let's export to CSV now
-devices.to_csv('devices.csv')
-print('Saved to \'devices.csv\'')
+    payload = {
+        "criteria": {
+            "deployment_type": [
+                "ENDPOINT",
+                "WORKLOAD",
+                "VDI"
+            ]
+        },
+        "sort": [
+            {
+                "field": "last_contact_time",
+                "order": "DESC"
+            }
+        ],
+        "start": 1,
+        "rows": 10000,
+        "": ""
+    }
+
+    headers = {
+        "Content-Type": "application/json",
+        "X-Auth-Token": api_token
+    }
+
+    response = requests.request("POST", req_url, headers=headers, json=payload)
+
+    if response.status_code == 200:
+        print(f"Success {response}")
+        devices_dict = response.json()
+        devices = pd.DataFrame.from_dict(devices_dict['results'])
+        devices.set_index('device_owner_id', drop=True, inplace=True)
+
+        print('Total devices found: ', end="")
+        print(devices_dict.get('num_found'))
+
+        # Cool. Let's export to CSV now
+        devices.to_csv('devices.csv')
+        print('Saved to \'devices.csv\'')
+
+    else:
+        print(response)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
