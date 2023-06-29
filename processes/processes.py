cceb142a49f0c1f76846e48507a82c77f7d7a793
nerdily <bmwftw@icloud.com>
2023-06-29 11:30:42 -0400

    Renamed scripts to match APIs they're using

--- processes/processes.py
+++ processes/processes.py
@@ -4,20 +4,23 @@ import argparse
 import sys
 
 
-# This script exports the queried for events into a csv via the process search API. To change the query, look at the "payload" variable which is the
+# This script exports the queried for processes into a csv via the process search API. To change the query, look at the "payload" variable which is the
 # json-formatted request made to the CB Cloud back end. The developer documentation has a full list of what can be queried.
 # The CB Cloud API will return up to 10,000 items in a single request. If you have more than 10,000 alerts, you would need
 # multiple requests to fetch them all.
 
-# This is an example of requesting additional fields that normally might need a second query to retrieve after the summary request.  Fields marked with "Process***" here:
+# This is a multistep query. A query is submitted which results in a job ID. That job ID gets polled and when completed the results are returned.
+# Then for each result, a second query is made for details via the process guid.
+
+# NOTE: There are some fields that do not require the second query. Fields marked with "Process***" here:
 # https://developer.carbonblack.com/reference/carbon-black-cloud/platform/latest/platform-search-fields/
 # can simply be added to in the initial request making the follow-up request unnecessary.
 
 # Usage: python export-alerts.py --help
 
 # API key permissions required:
-# Search - Events - org.search.events - CREATE
-# Search - Events - org.search.events - READ
+# Search - Events - org.search.processes - CREATE
+# Search - Events - org.search.processes - READ
 
 def get_environment(environment):
     # Function to get the required environment to build a Base URL. More info about building a Base URL can be found at
@@ -64,6 +67,15 @@ def build_search_job_id_url(environment, org_key, job_id):
     environment = get_environment(environment)
     return f"{environment}/api/investigate/v2/orgs/{org_key}/processes/detail_jobs/{job_id}/results"
 
+def build_process_detail_url(environment, org_key):
+    # Build the URL to return the process details job_id on process_guid
+    # Documentation on this specific API call can be found here:
+    # https://developer.carbonblack.com/reference/carbon-black-cloud/platform/latest/platform-search-api-processes/#request-details-of-processes-v2
+
+    # rtype: string
+    environment = get_environment(environment)
+    return f"{environment}/api/investigate/v2/orgs/{org_key}/processes/detail_jobs"
+
 
 def main():
     # Main function to parse arguments and retrieve the endpoint results
@@ -94,7 +106,7 @@ def main():
       },
       "rows": 10000,
       "fields": [
-        "*","process_cmdline"
+        "*"
       ],
       "sort": [
         {
@@ -129,9 +141,46 @@ def main():
             completed = response['completed']
         events = pd.DataFrame.from_dict(response['results'])
 
+        # Trim down the dataframe to just the fields we need
+        events = events[['process_guid', 'backend_timestamp', 'device_id', 'device_name', 'device_policy_id', 'process_name', 'process_username']]
+        # Add a process_cmdline column
+        events['process_cmdline'] = ''
+
+        # Iterate through the processes dataframe and do API call for each process_guid. For each process_guid we request a job_id.
+        # Then check status of the job_id. If completed = contacted, return the info.
+        for i, row in events.iterrows():
+            req_url = build_process_detail_url(args.environment, args.org_key)
+            process_guid = row.process_guid
+            payload = {
+              "process_guids": [
+                row.process_guid
+              ],
+              "limited": True
+            }
+            response = requests.request("POST", req_url, headers=headers, json=payload)
+            if response.status_code == 200:
+                print(f"Process detail success {response}")
+                response = response.json()
+                job_id = response['job_id']
+
+                # Now that we have the job_id, check the status of it:
+                req_url = build_search_job_id_url(args.environment, args.org_key, job_id)
+
+                # Reset contacted and completed to different values so the while loop kicks off at least once
+                contacted = 1
+                completed = -1
+                while completed != contacted:
+                    response = requests.request("GET", req_url, headers=headers)
+                    response = response.json()
+                    contacted = response['contacted']
+                    completed = response['completed']
+                details = pd.DataFrame.from_dict(response['results'])
+                events.at[i, 'process_cmdline'] = str(details.iloc[0]['process_cmdline'])
+                print("Event details successful - round " + str(i) )
+        print('Job complete')
         # Cool. Let's export to CSV now
-        events.to_csv('events.csv')
-        print('Saved to \'events.csv\'')
+        events.to_csv('processes.csv')
+        print('Saved to \'processes.csv\'')
     else:
         print(response)
 
