20a70e87ac4f622b9a6d1de06f04042edeeb3759
nerdily <bmwftw@icloud.com>
2023-09-11 07:33:51 -0400

    Updated alerts v7 export to handle more than 10,000 results.

--- alerts/export-alerts-v7.py
+++ alerts/export-alerts-v7.py
@@ -2,7 +2,9 @@ import pandas as pd
 import requests
 import argparse
 import sys
-
+import json
+from datetime import datetime, timedelta
+import time
 
 # This script exports the queried for alerts into a csv via the alerts v7 API. To change the query, look at the "payload" variable which is the
 # json-formatted request made to the CB Cloud back end. The developer documentation has a full list of what can be queried.
@@ -39,6 +41,15 @@ def get_environment(environment):
     elif environment == "GOVCLOUD":
         return "https://gprd1usgw1.carbonblack-us-gov.vmware.com"
 
+def setup_session(api_secret,api_id):
+    s = requests.session()
+    headers = {
+        "X-Auth-Token": f"{api_secret}/{api_id}",
+        "Content-Type": "application/json"
+        }
+    s.headers.update(headers)
+    return s
+
 
 def build_base_url(environment, org_key):
     # Build the base URL
@@ -50,11 +61,32 @@ def build_base_url(environment, org_key):
     environment = get_environment(environment)
     return f"{environment}/api/alerts/v7/orgs/{org_key}/alerts/_search"
 
+def request_data(session, pd, environment, org_id):
+    url = build_base_url(environment, org_id)
+    r = session.post(url, json=pd)
+    if r.status_code < 300:
+        results = r.json()
+    else:
+        results = False
+    return results
+
+
+def get_date_range(days_to_export):
+    chunks = 6
+    tomorrow = datetime.combine(datetime.now() + timedelta(days=1), datetime.min.time())
+    two_years_ago = tomorrow - timedelta(days=days_to_export)
+    dates = [tomorrow - timedelta(days=x) for x in range(days_to_export, 0, chunks * -1)] + [tomorrow]
+    i = 0
+    for date in dates:
+        dates[i] = datetime.strftime(date, '%Y-%m-%dT%H:%M:%S.%fZ')
+        i += 1
+    return dates
+
 
 def main():
     # Main function to parse arguments and retrieve the endpoint results
 
-    parser = argparse.ArgumentParser(prog="export-endpoints.py",
+    parser = argparse.ArgumentParser(prog="export-alerts-v7.py",
                                      description="Query VMware Carbon Black \
                                          Cloud for alert v7 data.")
     requiredNamed = parser.add_argument_group('required arguments')
@@ -69,52 +101,50 @@ def main():
                                help="API ID")
     requiredNamed.add_argument("-s", "--api_secret", required=True,
                                help="API Secret Key")
+    requiredNamed.add_argument("-d", "--days_to_export", required=True, help="Days to export")
     args = parser.parse_args()
 
-    req_url = build_base_url(args.environment, args.org_key)
-    api_token = f"{args.api_secret}/{args.api_id}"
+    dates = get_date_range(int(args.days_to_export))
+    session = setup_session(args.api_secret,args.api_id)
 
     payload = {
         "time_range": {
-            "range": "all"
         },
         "criteria": {
             "minimum_severity": 1,
+            "workflow_status":
+                [
+                    "OPEN",
+                    "IN_PROGRESS",
+                    "CLOSED"
+                ],
         },
         "start": "1",
         "rows": "10000",
         "sort": [
             {
-                "field": "severity",
+                "field": "backend_timestamp",
                 "order": "DESC"
             }
         ]
     }
 
-    headers = {
-        "Content-Type": "application/json",
-        "X-Auth-Token": api_token
-    }
+    alerts_json = []
+    for x, i in enumerate(dates):
+        if x + 1 < len(dates):
+            payload["time_range"]["start"] = dates[x]
+            payload["time_range"]["end"] = dates[x + 1]
+            print(payload)
+            data = request_data(session, payload, args.environment, args.org_key)
+            alerts_json.extend(data["results"])
+    with open("alerts.json", "w") as f:
+        json.dump(alerts_json, f)
+    # Now pull the json into a pandas dataframe as it can export to csv very nicely:
+    alerts_df = pd.read_json("alerts.json")
+    timestamp = time.strftime("%Y%m%d-%H%M%S") # create a timestamp for our filename
+    alerts_df.to_csv('alerts-v7-' + timestamp + '.csv' )
+    print('Saved to \'alerts-v7-' + timestamp + ' .csv\'') # let the user know
 
-    response = requests.request("POST", req_url, headers=headers, json=payload)
-
-    if response.status_code == 200:
-        print(f"Success {response}")
-
-        alerts_dict = response.json()
-
-        print('Total alerts found: ', end="")
-        print(alerts_dict.get('num_found'))
-        print('Total alerts available: ', end="")
-        print(alerts_dict.get('num_available'))
-
-        alerts = pd.DataFrame.from_dict(alerts_dict['results'])
-
-        # Cool. Let's export to CSV now
-        alerts.to_csv('alerts-v7.csv')
-        print('Saved to \'alerts-v7.csv\'')
-    else:
-        print(response)
 
 
 if __name__ == "__main__":
